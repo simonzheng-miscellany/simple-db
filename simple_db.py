a1b1f1114d51cef65c396e0e0e5936acf704f417
Simon Zheng <simonzheng@stanford.edu>
2015-02-02 21:48:54 +0800

    cleared debug statements and added whitespace for readability

--- simple_db.py
+++ simple_db.py
@@ -13,8 +13,7 @@ class SimpleDB:
     self.update_log = [] # We use update_log as a Python stack to track all old values that get changed within each transaction block
     self.value_counts = Counter()
 
-  def set(self, name, new_val, is_rollback=False):
-    print 'User has chosen to SET var ', name, ' to: ', new_val
+  def set(self, name, new_val, is_rollback=False):print 'User has chosen to SET var ', name, ' to: ', new_val
     # Get previous value
     old_val = None
     if name in self.data:
@@ -35,27 +34,28 @@ class SimpleDB:
       self.value_counts[old_val] -= 1
     self.value_counts[new_val] += 1
 
+
   def get(self, name):
-    print 'User has chosen to GET var ', name
     if name in self.data:
       print self.data[name]
     else:
       print 'NULL'
 
+
   def unset(self, name):
-    print 'User has chosen to UNSET var ', name
     if name in self.data:
       self.set(name, None)
     else:
       print 'ERROR: trying to unset variable that was never set'
 
+
   def get_num_equal_to(self, val):
-    print 'User has chosen to find NUMEQUALTO ', val
     print self.value_counts[val]
+
 
   def start_transaction(self):
     self.update_log.append({})
-    print 'User chose to begin a new transaction block, so there are now ', len(self.update_log), ' blocks'
+
 
   def rollback(self):
     # Iterate through recent transaction block and reset changed values
@@ -65,12 +65,11 @@ class SimpleDB:
       self.update_log.pop()
     else:
       print 'NO TRANSACTION'
-    print 'User chose to rollback to previous transaction block, so there are now ', len(self.update_log), ' blocks'
 
 
   def commit(self):
     self.update_log = []
-    print 'User chose to commit current transaction block, so there are now ', len(self.update_log), ' blocks'
+    
 
 def main():
   db = SimpleDB()
